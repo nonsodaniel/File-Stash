cad4669c4c88553af9917d3eebfcf75bdd37baa0
nonsodaniel <nonsodaniel07@gmail.com>
2023-09-27 23:43:46 +0100

    Created sidebar nav layout

---
+++ components/SideNavBar.tsx
@@ -0,0 +1,98 @@
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { menuList } from "../utils/constant";
+
+const SideNavBar = () => {
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+  const router = useRouter();
+  const onMenuClick = (item, index) => {
+    setActiveIndex(index);
+    router.push("/");
+  };
+  return (
+    <div
+      className="w-[200px]
+    bg-white h-screen sticky top-0
+    z-10 shadow-blue-200 shadow-md
+    p-5"
+    >
+      <div className="flex justify-center">
+        <span className="text-red" onClick={() => router.push("/")}>
+          File Stash
+        </span>
+      </div>
+      <button
+        className="flex gap-2 items-center text-[13px]
+        bg-blue-500 p-2 text-white rounded-md px-3
+        hover:scale-105 transition-all mt-5 w-full justify-center"
+      >
+        Add New File
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      </button>
+      <button
+        className="flex gap-2 items-center text-[13px]
+        bg-sky-400 w-full p-2 justify-center text-white rounded-md px-3
+        hover:scale-105 transition-all mt-1"
+      >
+        Create Folder
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      </button>
+      <div className="mt-7">
+        {menuList.map((item, index) => (
+          <h2
+            key={index}
+            className={`flex gap-2 items-center
+            p-2 mt-3 text-gray-500 rounded-md cursor-pointer
+            hover:bg-blue-500 hover:text-white
+            ${activeIndex == index ? "bg-blue-500 text-white" : null}`}
+            onClick={() => onMenuClick(item, index)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d={item.logo}
+              />
+            </svg>
+            {item.name}
+          </h2>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SideNavBar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
+import SideNavBar from "../components/SideNavBar";
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <SideNavBar />
       <Component {...pageProps} />
     </SessionProvider>
   );
