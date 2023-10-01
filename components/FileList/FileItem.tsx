fbcb9332ef1b1931b94ec57847f72ecfcab0ece6
nonsodaniel <nonsodaniel07@gmail.com>
2023-10-01 02:14:18 +0100

    Implement delete action for files

--- components/FileList/FileItem.tsx
+++ components/FileList/FileItem.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import moment from "moment/moment";
 import Image from "next/image";
+import { ShowToastContext } from "../../context/ShowToastContext";
+import { deleteDoc, doc, getFirestore } from "firebase/firestore";
+import { app } from "../../config/firebaseConfig";
 
 const FileItem = ({ file }) => {
   const image = "/" + file.type + ".png";
+  const db = getFirestore(app);
+
+  const { showToastMessage, setShowToastMessage } =
+    useContext(ShowToastContext);
+  const deleteFile = async (e) => {
+    const id = e.target.id;
+    console.log(id);
+    await deleteDoc(doc(db, "files", id.toString())).then((resp) => {
+      setShowToastMessage("File Successfully Deleted!!!");
+    });
+  };
 
   return (
     <div
@@ -33,7 +47,10 @@ const FileItem = ({ file }) => {
 
         <h2 className="text-[15px]">
           {(file.size / 1024 ** 2).toFixed(2) + " MB"}
+        </h2>
+        <span onClick={deleteFile} id={file.id}>
           <svg
+            id={file.id}
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
@@ -48,7 +65,7 @@ const FileItem = ({ file }) => {
               d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
             />
           </svg>
-        </h2>
+        </span>
       </div>
     </div>
   );
