fb8eab7791e89679cf0b7f8a28c104e31c52a999
nonsodaniel <nonsodaniel07@gmail.com>
2023-10-14 01:10:24 +0100

    Fix: Fixed SideNav active link view

--- components/ui/SideNavBar.tsx
+++ components/ui/SideNavBar.tsx
@@ -65,7 +65,7 @@ const SideNavBar = () => {
               item.isSoon ? "pointer-events-none" : ""
             }
             hover:bg-blue-500 hover:text-white
-            ${activeIndex == index ? "bg-blue-500 text-white" : null}`}
+            ${router.pathname == item.url ? "bg-blue-500 text-white" : null}`}
             onClick={() => onMenuClick(item, index)}
           >
             <span style={{ fontSize: "25px" }}>{item.logo}</span>
