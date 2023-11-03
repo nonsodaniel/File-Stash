537da1c6b435f453b8838506afd843943d479c16
nonsodaniel <nonsodaniel07@gmail.com>
2023-11-04 00:00:50 +0100

    Show correct storage Data

--- components/Storage/Storage.tsx
+++ components/Storage/Storage.tsx
@@ -1,30 +1,28 @@
-import React, { useContext } from "react";
+import React from "react";
 import UserInformation from "./UserInformation";
 import StorageInformation from "./StorageInformation";
 import { useSession } from "next-auth/react";
 import StorageDetailList from "./StorageDetailsList";
-import StorageUpgradeMessage from "./StorageUpgradeMessage";
-import { DataContext } from "../../context/DataContext";
-import useFolderList from "../../hooks/useFolderList";
+// import StorageUpgradeMessage from "./StorageUpgradeMessage";
 import useFileList from "../../hooks/useFileList";
+import Loader from "../ui/Loader";
 
 const Storage = () => {
   const { data: session } = useSession();
-  const { isFolderLoading, folderList, fetchFolderList } = useFolderList();
-  const { isFileLoading, fileList } = useFileList();
-  console.log("storage", { fileList, folderList });
+  const { isFileLoading, storageData } = useFileList();
+
   return (
     session && (
       <div>
         <UserInformation />
-        {!!fileList.length && (
+        {!!storageData.length && (
           <>
-            <StorageInformation fileList={fileList} />
-            <StorageDetailList fileList={fileList} />
+            <StorageInformation fileList={storageData} />
+            <StorageDetailList fileList={storageData} />
           </>
         )}
-
-        <StorageUpgradeMessage />
+        {isFileLoading && <Loader />}
+        {/* <StorageUpgradeMessage /> */}
       </div>
     )
   );
