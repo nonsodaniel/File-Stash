9f0994e1169ca9d3e72d3c7c6d4fdf58a169740c
nonsodaniel <nonsodaniel07@gmail.com>
2023-11-05 19:48:58 +0100

    Make Profile page inacessible on large screens

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppLayout from "../../components/layout/AppLayout";
 import Storage from "../../components/Storage/Storage";
+import { useRouter } from "next/router";
 
 const Home = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (screen.width > 1042) {
+      router.push("/");
+    }
+  }, []);
   return (
     <AppLayout>
       <div className={"p-5"}>
