c3aace59cc5590cf09ea4e11261855496f509f41
nonsodaniel <nonsodaniel07@gmail.com>
2023-11-05 21:13:45 +0100

    Added back button on profile page

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import AppLayout from "../../components/layout/AppLayout";
 import Storage from "../../components/Storage/Storage";
 import { useRouter } from "next/router";
+import { BiArrowBack } from "react-icons/bi";
 
 const Home = () => {
   const router = useRouter();
@@ -14,6 +15,12 @@ const Home = () => {
   return (
     <AppLayout>
       <div className={"p-5"}>
+        <div className="pb-8">
+          <BiArrowBack
+            className="mr-4 text-gray-800 cursor-pointer text-2xl"
+            onClick={() => router.back()}
+          />
+        </div>
         <Storage />
       </div>
     </AppLayout>
